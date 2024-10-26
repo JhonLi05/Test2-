cf6a8dacff3b897abbf50d3b053c91b479dd0472
Jhonny Corrales <jhonzc.7@gmail.com>
2024-10-25 21:55:53 -0600

    MIA

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,13 @@ public class Main {
         Gatos g1 = new Gatos(3,"Zeus");
         Gatos g2 = new Gatos(4,"Liam");
         Gatos g3 = new Gatos(5,"Bigo");
+        Gatos g4 = new Gatos(6,"MIA");
 
         //System.out.println(g1);
         System.out.printf(g1.toString()).println();
         System.out.printf(g2.toString()).println();
-        System.out.printf(g3.toString());
+        System.out.printf(g3.toString()).println();
+        System.out.printf(g4.toString()).println();
 
         }
     }
