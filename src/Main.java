20f04485db245b277c1eb7290aa05c6a23ae5b23
Jhonny Corrales <jhonzc.7@gmail.com>
2024-10-25 21:50:47 -0600

    pass

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Gatos g1 = new Gatos(3,"Zeus");
         Gatos g2 = new Gatos(4,"Liam");
         Gatos g3 = new Gatos(5,"Bigo");
-        
+
         //System.out.println(g1);
         System.out.printf(g1.toString()).println();
         System.out.printf(g2.toString()).println();
