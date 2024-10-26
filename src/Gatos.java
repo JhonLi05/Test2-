6601e3a036bfddb927738f60a0bef0f5e4e8927c
Jhonny Corrales <jhonzc.7@gmail.com>
2024-10-25 21:33:20 -0600

    FirstCommit

---
+++ src/Gatos.java
@@ -0,0 +1,28 @@
+public class Gatos {
+    public int edad;
+    public String nombre;
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {return edad;}
+    public String getNombre() {return nombre;}
+
+    public Gatos(int edad,String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Gatos{" +
+                "edad=" + getEdad() +
+                ", nombre='" + getNombre() + '\'' +
+                '}';
+    }
+}
